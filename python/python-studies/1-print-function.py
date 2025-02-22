16ee9788f994f5fcb3115f54fddc07578910f40a
Gillian Franco <gillian.melo.franco17@gmail.com>
2025-02-21 22:01:15 -0300

    Adicionando `1-print-function.py`

---
+++ python/python-studies/1-print-function.py
@@ -0,0 +1,22 @@
+# Função de saída
+
+# Imprime Hello World!
+print('Hello, World!')
+
+# Imprime Hello World!
+print('Hello, ' + 'World!')
+
+# Imprime Hello World!
+print('Hello,', 'World!')
+ 
+# Imprime 17
+print(10 + 7)
+
+# Imprime 10 + 7
+print('10 + 7')
+
+# Imprime 107
+print('10' + '7')
+
+# The result of 10 + 7 is 17
+print('The result of 10 + 7 is', 10 + 7, '\n')
