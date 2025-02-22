a79cf22ec01b44a5e48e384cd7e302493404e4e5
Gillian Franco <gillian.melo.franco17@gmail.com>
2025-02-21 22:04:17 -0300

    Adicionando `4-slicing.py`

---
+++ python/python-studies/4-slicing.py
@@ -0,0 +1,10 @@
+# Fatiamento de strings
+
+s1 = 'Hello, World!'
+
+print('Primeiro caractere: ' + s1[0]) # Primeira letra da string
+print('Primeira palavra: ' + s1[0:5])
+print('Último caractere: ' + s1[-1])
+print('Última palavra: ' + s1[7:12])
+print('Primeiros 3 caracteres: ' + s1[:3])
+print('Últimos 3 caractere: ' + s1[-3:])
