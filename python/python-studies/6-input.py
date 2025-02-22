3ac4363f0262fbcb13cbc6ec3f1c2bb6e4e57f6c
Gillian Franco <gillian.melo.franco17@gmail.com>
2025-02-22 00:47:49 -0300

    Adicionando `6-input.py`

---
+++ python/python-studies/6-input.py
@@ -0,0 +1,14 @@
+# Entrada de string
+
+nome = input('Qual é o seu nome? ')
+print(f'Seu nome é {nome}\n')
+
+# Entrada de string e conversão para inteiro
+
+idade = int(input('Qual é a sua idade? '))
+print(f'Sua idade é {idade}\n')
+
+# Entrada de string e conversão para float
+
+altura = float(input('Qual a sua altura? '))
+print(f'Sua altura é {altura}\n')
