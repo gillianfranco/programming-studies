ab5c08a28a9a1c9d8113576d82364e059c78b9d5
Gillian Franco <gillian.melo.franco17@gmail.com>
2025-02-21 22:15:17 -0300

    Corregindo erro de português

--- python/python-studies/5-len-function.py
+++ python/python-studies/5-len-function.py
@@ -1,4 +1,4 @@
-# Abreviação de lenght - Indica o tamnaho de uma string
+# Abreviação de lenght - Indica o tamanho de uma string
 
 s1 = 'Lógica de Programação e Algoritmos'
 print('A string s1 possui', len(s1), 'caracteres.')
