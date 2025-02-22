5f81accb76ac18491fda2c561531b59615e2acc8
Gillian Franco <gillian.melo.franco17@gmail.com>
2025-02-21 22:01:57 -0300

    Adicionando `2-variables.py`

---
+++ python/python-studies/2-variables.py
@@ -0,0 +1,22 @@
+# Declaração e atribuição de uma variável
+nota1 = 8
+
+# Declaração e atribuição de duas variáveis
+nota2, nota3 = 4, 5
+
+# Declaração de uma string
+msgm = 'Sua nota total foi'
+
+# Impressão de todas as variáveis
+print(msgm, nota1 + nota2 + nota3)
+
+
+# Nomes válidos
+
+idade = 19 # Forma recomendada
+nota4 = 10
+menor_nota = 2 # Forma recomendada
+Maior_nota = 10
+_menor_ = 2
+adicao = 71
+adição = 17 # A partir do Python 3.0
