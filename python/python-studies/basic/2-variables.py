0117413215d051c5431f510dc36d9eee7ebfc68c
Gillian Franco <gillian.melo.franco17@gmail.com>
2025-02-23 15:41:53 -0300

    Atualizando `2-variables.py`

--- python/python-studies/basic/2-variables.py
+++ python/python-studies/basic/2-variables.py
@@ -1,22 +1,25 @@
 # Declaração e atribuição de uma variável
 nota1 = 8
 
-# Declaração e atribuição de duas variáveis
-nota2, nota3 = 4, 5
+# Declaração e atribuição de duas ou mais variáveis
+nota2, nota3 = 4, 5 
 
 # Declaração de uma string
-msgm = 'Sua nota total foi'
+msgm = 'Sua nota total foi' # Também pode ser usado aspas duplas
 
 # Impressão de todas as variáveis
 print(msgm, nota1 + nota2 + nota3)
 
+# Anulação da variável
+nota1 = None
+print(nota1) # Imprime None
 
 # Nomes válidos
-
-idade = 19 # Forma recomendada
+idade = 19 # Forma recomendada - snake_case
 nota4 = 10
 menor_nota = 2 # Forma recomendada
 Maior_nota = 10
+notaMedia = (2 + 10) / 2
 _menor_ = 2
 adicao = 71
 adição = 17 # A partir do Python 3.0
