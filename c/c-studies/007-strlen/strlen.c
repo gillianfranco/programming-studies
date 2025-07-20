88fe884ad0f589ffdc37bf92c4b30ee269f7fd3b
Gillian <gillian.melo.franco17@gmail.com>
2025-07-20 17:34:36 -0300

    add `007-strlen`

---
+++ c/c-studies/007-strlen/strlen.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <string.h> // Biblioteca para funções string
+
+int main(){
+    char nome[] = "Gillian";
+    printf("Meu nome tem %lu letras\n", strlen(nome));
+
+    return 0;
+}
