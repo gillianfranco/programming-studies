87f66aa11dbd4d831ad90cc6e299928cb4455166
Gillian <gillian.melo.franco17@gmail.com>
2025-07-20 17:30:49 -0300

    add `001-print-function`

---
+++ c/c-studies/001-print-function/print-function.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    printf("Hello, World!\n"); // Função de saída
+
+    printf("Hello, %s\n", "World!"); // Com marcador de posição %s
+    
+    printf("10 + 7 = %d\n", 10 + 7); // Impressão do resultado da soma
+
+    printf("%d%d\n", 10, 7); // Imprime 107
+
+    return 0;
+}
