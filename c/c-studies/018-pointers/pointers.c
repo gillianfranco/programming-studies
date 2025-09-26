3cc67ad810c7aa4a0332e1f195651ae3e4894f16
Gillian <gillian.melo.franco17@gmail.com>
2025-09-26 11:55:50 -0300

    Update `018-pointers/`

--- c/c-studies/018-pointers/pointers.c
+++ c/c-studies/018-pointers/pointers.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-void calculate(int* n){
-    (*n)++;
-}
+void calculate(int* n);
+
+struct Person {
+    char name[50];
+    int age;
+};
 
 int main(){
     int myAge = 19;
@@ -15,6 +20,8 @@ int main(){
     printf("'myAge' value from 'ptr': %d\n", *ptr); // Impressão do valor de 'myAge'
     printf("'myAge' address from 'ptr': %p\n\n", ptr); // Impressão do endereço de 'myAge'
     
+
+
     // Passagem por referência
 
     int number = 10;
@@ -24,32 +31,67 @@ int main(){
     calculate(&number);
     printf("After: %d\n\n", number);
 
-    // Ponteiro como array
+
+
+    // Ponteiro como vetor
 
     int myNumbers[4] = {25, 50, 75, 100};
     
-    // Endereço da posição 0
+    printf("%p\n", &myNumbers); // Endereço da posição 0. Mesma coisa usando `&myNumbers[0]`
 
-    printf("%p\n", &myNumbers);
-    printf("%p\n\n", &myNumbers[0]);
+    int* ptr2 = &myNumbers[0]; // Ponteiro que aponta para a primeira posição do vetor
 
-    // Valor de cada posição com ponteiro
-
-    int* ptr2 = &myNumbers[0];
-
-    for(int i = 0; i < 4; i++){
-        printf("%d\n", *(ptr2 + i));
+    for(int i = 0; i < 4; i++) {
+        printf("%d\n", *(ptr2 + i)); // Impressão dos elementos a partir da soma do endereço de cada posição
     }
+
+
+
+    // Ponteiro como matriz
+
+    int matriz[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+
+    printf("\n%p\n", &matriz);
+
+    int *ptr3 = &matriz[0][0];
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("%d\t", *ptr3); // Impressão do elemento atual
+            ptr3++; // Soma no endereço
+        }
+        printf("\n");
+    }
+
+
+
+    // Ponteiro como struct
+
+    struct Person person1;
+    strcpy(person1.name, "Gillian");
+    person1.age = 19;
+
+    struct Person *ptr_person1;
+    ptr_person1 = &person1;
+
+    printf("\n%s\n", ptr_person1->name);
+    printf("%d\n", ptr_person1->age);
+
+
 
     // Ponteiro de ponteiro
 
     int x = 17;
-    int *ponteiro_x = &x;
-    int **ponteiro_ponteiro_x = &ponteiro_x; 
+    int *ptr_x = &x;
+    int **ptr_ptr_x = &ptr_x; 
     
-    printf("%d\n", x);
-    printf("%d\n", *ponteiro_x);
-    printf("%d\n", **ponteiro_ponteiro_x);
+    printf("\n%d\n", x);
+    printf("%d\n", *ptr_x);
+    printf("%d\n", **ptr_ptr_x);
 
     return 0;
-}   
+}
+
+void calculate(int* n){
+    (*n)++;
+}
